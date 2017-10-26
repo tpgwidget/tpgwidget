dbb730e9d0921d958b0ae5d926e4d964cb6bb15b
Nicolapps <webmaster@nicolapps.ch>
2017-10-26 22:42:49 +0200

    Fix iOS install

--- tpg/app/install.php
+++ tpg/app/install.php
@@ -1,7 +1,7 @@
-<?php
-require '../../tpgdata/stops.php';
+<?
+include '../stops.php';
 
-if(!isset($_GET["id"])) {
+if(!isset($_GET["id"])){
     $erreur = "Paramètre manquant";
 } else {
     require '../../tpgdata/apikey.php';
@@ -9,80 +9,85 @@ if(!isset($_GET["id"])) {
     $nextDepartures = @simplexml_load_file($file);
 }
 
-if ($nextDepartures){
+if($nextDepartures){
 ?>
+<div class="navbar">
+  <div class="navbar-inner">
+    <div class="left">
+      <a href="#" class="back link">
+        <i class="icon icon-back"></i>
+        <span>Retour</span>
+       </a>
+    </div>
+    <div class="center sliding"><?=$nextDepartures->stop->stopName?></div>
+  </div>
+</div>
+<div class="pages">
   <div data-page="install" class="page page-page">
-      <div class="navbar">
-          <div class="navbar-inner">
-            <div class="left">
-              <a href="#" class="back link icon-only">
-                <i class="icon icon-back"></i>
-               </a>
-            </div>
-            <div class="center"><?= stopFilter($nextDepartures->stop->stopName) ?></div>
-          </div>
-        </div>
-
     <div class="page-content">
-        <div class="content-block">
-            <ul class="lignes">
-            <?
-            $lignes = [];
+        <div class="card">
+            <div class="card-header"><?=$nextDepartures->stop->stopName?></div>
+            <div class="card-content">
+            <div class="card-content-inner">
+                <ul class="lignes">
+                <?
+                $lignes = [];
 
-               foreach($nextDepartures->stop->connections->connection as $connection){
-                   $lignes[] = $connection->lineCode;
-               }
-
-               $lignes = array_unique($lignes);
-               $lignesNocambus = [];
-
-               foreach($lignes as $key => $ligne){
-                   if(substr($ligne, "0", "1") == "N"){
-                        $lignesNocambus[] = $ligne;
-                        unset($lignes[$key]);
+                   foreach($nextDepartures->stop->connections->connection as $connection){
+                       $lignes[] = $connection->lineCode;
                    }
-               }
 
-               foreach($lignes as $ligne){
-                   echo '<li class="l'.$ligne.'">'.$ligne.'</li>';
-               }
+                   $lignes = array_unique($lignes);
 
-               foreach($lignesNocambus as $ligne){
-                   echo '<li class="l'.$ligne.'">'.$ligne.'</li>';
-               }
-            ?>
-            </ul>
-        </div>
-        <div class="content-block">
-            <a href="googlechrome://navigate?url=https://tpga.nicolapps.ch/<?=$nextDepartures->stop->stopCode?>/" class="button button-big button-fill external button-install" data-tapped="0">Installer</a></div>
-        </div>
-        <div class="content-block install-chrome">
-            <h1>Installez Google Chrome</h1>
+                   foreach($lignes as $key => $ligne){
+                       if(substr($ligne, "0", "1") == "N"){
+                            $lignesNocambus[] = $ligne;
+                            unset($lignes[$key]);
+                       }
+                   }
+
+                   foreach($lignes as $ligne){
+                       echo '<li class="l'.$ligne.'">'.$ligne.'</li>';
+                   }
+
+                   foreach($lignesNocambus as $ligne){
+                       echo '<li class="l'.$ligne.'">'.$ligne.'</li>';
+                   }
+                ?>
+                </ul>
+            </div>
+            </div>
+            <div class="card-footer"><a href="#" data-stop="<?=$nextDepartures->stop->stopCode?>" class="button button-big button-fill install">Installer</a></div>
         </div>
     </div>
   </div>
+</div>
 <?
 } else {
 ?>
-  <div data-page="install" class="page page-page layout-dark">
-    <div class="navbar">
-      <div class="navbar-inner">
-        <div class="left">
-          <a href="#" class="back link icon-only">
-            <i class="icon icon-back"></i>
-           </a>
-        </div>
-        <div class="center">Erreur</div>
-      </div>
+<div class="navbar">
+  <div class="navbar-inner">
+    <div class="left">
+      <a href="#" class="back link">
+        <i class="icon icon-back"></i>
+        <span>Retour</span>
+       </a>
     </div>
-
+    <div class="center sliding">Erreur</div>
+    <div class="right">
+      <a href="#" class="link icon-only open-panel"><i class="icon icon-bars-blue"></i></a>
+    </div>
+  </div>
+</div>
+<div class="pages">
+  <div data-page="install" class="page page-page layout-dark">
     <div class="page-content">
         <section class="graym">
 		    <h1>:-(</h1>
 		    <h2>Erreur : <?
     		    if($erreur){
         		    print $erreur;
-    		    } else {
+    		    }else{
         		    print "Serveur TPG indisponible";
     		    }
 
@@ -90,4 +95,5 @@ if ($nextDepartures){
 	    </section>
     </div>
   </div>
+</div>
 <? } ?>
