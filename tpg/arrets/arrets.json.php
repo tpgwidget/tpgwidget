afec01fe218831315b7ae2da04be205225fb0d6c
Nicolas Ettlin <webmaster@nicolapps.ch>
2018-01-05 17:32:37 +0100

    Sort stops list (closes #6)

--- tpg/arrets/arrets.json.php
+++ tpg/arrets/arrets.json.php
@@ -1,24 +1,19 @@
 <?php
-header('Content-type: application/json; charset=utf-8');
 
 require '../../tpgdata/apikey.php';
 require '../../tpgdata/stops.php';
 $stops = @simplexml_load_file('http://prod.ivtr-od.tpg.ch/v1/GetStops.xml?key='.$key);
+$output = [];
 
+if ($stops) {
+    foreach ($stops->stops->stop as $stop) {
+        $output[] = ['stopName' => stopFilter($stop->stopName), 'stopCode' => $stop->stopCode];
+    }
+}
 
-echo '[';
+usort($output, function($a, $b) {
+    return strcoll($a['stopName'], $b['stopName']);
+});
 
-  $firstStop = true;
-
-  foreach($stops->stops->stop as $stop) {
-      if(!$firstStop){
-          echo ', ';
-      }
-
-      if($firstStop) {
-        $firstStop = false;
-      }
-      echo '{ "stopName": "'.stopFilter($stop->stopName).'", "stopCode": "'.$stop->stopCode.'" }';
-  }
-
-echo ']';
+header('Content-Type: application/json');
+echo json_encode($output);
