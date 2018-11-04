c45a3468a00d0ead6281d34dc1ded41712af449d
Nicolas Ettlin <nicolas.ettlin@me.com>
2018-11-04 12:18:33 +0100

    Use tpgwidget/data for stop autocompletion

--- tpga/itineraire/stops.json.php
+++ tpga/itineraire/stops.json.php
@@ -1,21 +1,8 @@
 <?php
 require_once __DIR__.'/../../config.inc.php';
+use TPGwidget\Data\Datasets;
 header('Content-type: application/json; charset=utf-8');
 
-$i = 0;
-
-echo '[';
-
-    $req = $bdd->query('SELECT tpg FROM `tpg-sbb` ORDER BY tpg');
-
-    while($stop = $req->fetch()) {
-        if ($i !== 0) {
-            echo ',';
-        }
-
-        echo '"'.$stop['tpg'].'"';
-
-        $i++;
-    }
-
-echo ']';
+$stops = array_values(Datasets::load('stopTranslation'));
+sort($stops);
+echo json_encode($stops);
