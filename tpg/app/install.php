83117def912f85d2f6e650ac4e7ce046817dc3cd
Nicolas Ettlin <nicolas.ettlin@me.com>
2018-11-20 18:15:54 +0100

    Use TPGwidget\Data line colors on the install page

--- tpg/app/install.php
+++ tpg/app/install.php
@@ -1,99 +1,103 @@
 <?php
 require __DIR__.'/../../config.inc.php';
+use TPGwidget\Data\{Lines, Stops};
 
 if (!isset($_GET["id"])) {
     $erreur = "Paramètre manquant";
+    $nextDepartures = null;
 } else {
     $file = 'http://prod.ivtr-od.tpg.ch/v1/GetNextDepartures.xml?key='.getenv('TPG_API_KEY').'&stopCode=' . htmlentities($_GET["id"]);
     $nextDepartures = @simplexml_load_file($file);
 }
 
-if ($nextDepartures) {
-?>
-<div class="navbar">
-<div class="navbar-inner">
-    <div class="left">
-    <a href="#" class="back link">
-        <i class="icon icon-back"></i>
-        <span>Retour</span>
-    </a>
-    </div>
-    <div class="center sliding"><?=$nextDepartures->stop->stopName?></div>
-</div>
-</div>
-<div class="pages">
-<div data-page="install" class="page page-page">
-    <div class="page-content">
-        <div class="card">
-            <div class="card-header"><?=$nextDepartures->stop->stopName?></div>
-            <div class="card-content">
-            <div class="card-content-inner">
-                <ul class="lignes">
-                <?php
-                $lignes = [];
-
-                foreach ($nextDepartures->stop->connections->connection as $connection) {
-                    $lignes[] = $connection->lineCode;
-                }
-
-                $lignes = array_unique($lignes);
-                $lignesNoctambus = [];
-
-                foreach ($lignes as $index => $ligne) {
-                    if (substr($ligne, 0, 1) === 'N') {
-                            $lignesNoctambus[] = $ligne;
-                            unset($lignes[$index]);
-                    }
-                }
-
-                foreach ($lignes as $ligne) {
-                    echo '<li class="l'.$ligne.'">'.$ligne.'</li>';
-                }
-
-                foreach ($lignesNoctambus as $ligne) {
-                    echo '<li class="l'.$ligne.'">'.$ligne.'</li>';
-                }
-                ?>
-                </ul>
+if ($nextDepartures) { ?>
+    <div class="navbar">
+        <div class="navbar-inner">
+            <div class="left">
+            <a href="#" class="back link">
+                <i class="icon icon-back"></i>
+                <span>Retour</span>
+            </a>
             </div>
-            </div>
-            <div class="card-footer"><a href="#" data-stop="<?=$nextDepartures->stop->stopCode?>" class="button button-big button-fill install">Installer</a></div>
+            <div class="center sliding"><?= Stops::correct($nextDepartures->stop->stopName) ?></div>
         </div>
     </div>
-</div>
-</div>
+    <div class="pages">
+        <div data-page="install" class="page page-page">
+            <div class="page-content">
+                <div class="card">
+                    <div class="card-header"><?= Stops::correct($nextDepartures->stop->stopName) ?></div>
+                    <div class="card-content">
+                        <div class="card-content-inner">
+                            <ul class="lignes">
+                                <?php
+                                $lignes = [];
+
+                                foreach ($nextDepartures->stop->connections->connection as $connection) {
+                                    $lignes[] = $connection->lineCode;
+                                }
+
+                                $lignes = array_unique($lignes);
+                                $lignesNoctambus = [];
+
+                                foreach ($lignes as $index => $ligne) {
+                                    if (preg_match('/^N.$/', $ligne)) {
+                                        $lignesNoctambus[] = $ligne;
+                                        unset($lignes[$index]);
+                                    }
+                                }
+
+                                foreach (array_merge($lignes, $lignesNoctambus) as $ligne) {
+                                    $details = Lines::get($ligne);
+                                    ?>
+                                    <li style="background-color: <?= $details['background'] ?>; color: <?= $details['text'] ?>">
+                                        <?= $ligne ?>
+                                    </li>
+                                <?php } ?>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" data-stop="<?=$nextDepartures->stop->stopCode?>" class="button button-big button-fill install">
+                            Installer
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php } else { ?>
-<div class="navbar">
-<div class="navbar-inner">
-    <div class="left">
-    <a href="#" class="back link">
-        <i class="icon icon-back"></i>
-        <span>Retour</span>
-    </a>
+    <div class="navbar">
+        <div class="navbar-inner">
+            <div class="left">
+                <a href="#" class="back link">
+                    <i class="icon icon-back"></i>
+                    <span>Retour</span>
+                </a>
+            </div>
+            <div class="center sliding">Erreur</div>
+            <div class="right">
+                <a href="#" class="link icon-only open-panel"><i class="icon icon-bars-blue"></i></a>
+            </div>
+        </div>
     </div>
-    <div class="center sliding">Erreur</div>
-    <div class="right">
-    <a href="#" class="link icon-only open-panel"><i class="icon icon-bars-blue"></i></a>
+    <div class="pages">
+        <div data-page="install" class="page page-page layout-dark">
+            <div class="page-content">
+                <section class="graym">
+                    <h1>:-(</h1>
+                    <h2>
+                        Erreur :
+                        <?php
+                        if ($erreur) {
+                            print $erreur;
+                        } else {
+                            print "Serveur TPG indisponible";
+                        }
+                        ?>
+                    </h2>
+                </section>
+            </div>
+        </div>
     </div>
-</div>
-</div>
-<div class="pages">
-<div data-page="install" class="page page-page layout-dark">
-    <div class="page-content">
-        <section class="graym">
-            <h1>:-(</h1>
-            <h2>
-                Erreur :
-                <?php
-                if ($erreur) {
-                    print $erreur;
-                } else {
-                    print "Serveur TPG indisponible";
-                }
-                ?>
-            </h2>
-        </section>
-    </div>
-</div>
-</div>
-<?php } ?>
+<?php } ?>
--- tpga/app/install.php
+++ tpga/app/install.php
@@ -1,95 +1,100 @@
 <?php
 require_once __DIR__.'/../../config.inc.php';
-use TPGwidget\Data\Stops;
+use TPGwidget\Data\{Lines, Stops};
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 if (!isset($_GET["id"])) {
     $erreur = "Paramètre manquant";
+    $nextDepartures = null;
 } else {
     $file = 'http://prod.ivtr-od.tpg.ch/v1/GetNextDepartures.xml?key='.getenv('TPG_API_KEY').'&stopCode=' . htmlentities($_GET["id"]);
     $nextDepartures = @simplexml_load_file($file);
 }
 
-if ($nextDepartures) {
-?>
-<div data-page="install" class="page page-page">
-    <div class="navbar">
-        <div class="navbar-inner">
-            <div class="left">
-            <a href="#" class="back link icon-only">
-                <i class="icon icon-back"></i>
-            </a>
+if ($nextDepartures) { ?>
+    <div data-page="install" class="page page-page">
+        <div class="navbar">
+            <div class="navbar-inner">
+                <div class="left">
+                    <a href="#" class="back link icon-only">
+                        <i class="icon icon-back"></i>
+                    </a>
+                </div>
+                <div class="center"><?= Stops::correct($nextDepartures->stop->stopName) ?></div>
             </div>
-            <div class="center"><?= Stops::correct($nextDepartures->stop->stopName) ?></div>
-        </div>
         </div>
 
-    <div class="page-content">
-        <div class="content-block">
-            <ul class="lignes">
-            <?php
-            $lignes = [];
+        <div class="page-content">
+            <div class="content-block">
+                <ul class="lignes">
+                    <?php
+                    $lignes = [];
 
-            foreach ($nextDepartures->stop->connections->connection as $connection) {
-                $lignes[] = $connection->lineCode;
-            }
+                    foreach ($nextDepartures->stop->connections->connection as $connection) {
+                        $lignes[] = $connection->lineCode;
+                    }
 
-            $lignes = array_unique($lignes);
-            $lignesNoctambus = [];
+                    $lignes = array_unique($lignes);
+                    $lignesNoctambus = [];
 
-            foreach ($lignes as $index => $ligne) {
-                if (substr($ligne, 0, 1) === 'N') {
-                    $lignesNoctambus[] = $ligne;
-                    unset($lignes[$index]);
-                }
-            }
+                    foreach ($lignes as $index => $ligne) {
+                        if (preg_match('/^N.$/', $ligne)) {
+                            $lignesNoctambus[] = $ligne;
+                            unset($lignes[$index]);
+                        }
+                    }
 
-            foreach ($lignes as $ligne) {
-                echo '<li class="l'.$ligne.'">'.$ligne.'</li>';
-            }
-
-            foreach ($lignesNoctambus as $ligne) {
-                echo '<li class="l'.$ligne.'">'.$ligne.'</li>';
-            }
-            ?>
-            </ul>
-        </div>
-        <div class="content-block">
-            <a href="googlechrome://navigate?url=https://tpga.nicolapps.ch/<?=$nextDepartures->stop->stopCode?>/" class="button button-big button-fill external button-install" data-tapped="0">Installer</a></div>
-        </div>
-        <div class="content-block install-chrome">
-            <h1>Installez Google Chrome</h1>
+                    foreach (array_merge($lignes, $lignesNoctambus) as $ligne) {
+                        $details = Lines::get($ligne);
+                        ?>
+                        <li style="background-color: <?= $details['background'] ?>; color: <?= $details['text'] ?>">
+                            <?= $ligne ?>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+            <div class="content-block">
+                <a
+                    href="googlechrome://navigate?url=https://tpga.nicolapps.ch/<?=$nextDepartures->stop->stopCode?>/"
+                    class="button button-big button-fill external button-install"
+                    data-tapped="0"
+                >
+                    Installer
+                </a>
+            </div>
+            <div class="content-block install-chrome">
+                <h1>Installez Google Chrome</h1>
+            </div>
         </div>
     </div>
-</div>
 <?php } else { ?>
-<div data-page="install" class="page page-page layout-dark">
-    <div class="navbar">
-    <div class="navbar-inner">
-        <div class="left">
-        <a href="#" class="back link icon-only">
-            <i class="icon icon-back"></i>
-        </a>
+    <div data-page="install" class="page page-page layout-dark">
+        <div class="navbar">
+            <div class="navbar-inner">
+                <div class="left">
+                    <a href="#" class="back link icon-only">
+                        <i class="icon icon-back"></i>
+                    </a>
+                </div>
+                <div class="center">Erreur</div>
+            </div>
         </div>
-        <div class="center">Erreur</div>
-    </div>
-    </div>
 
-    <div class="page-content">
-        <section class="graym">
-            <h1>:-(</h1>
-            <h2>
-                Erreur :
-                <?php
-                if ($erreur) {
-                    print $erreur;
-                } else {
-                    print "Serveur TPG indisponible";
-                }
-                ?>
-            </h2>
-        </section>
+        <div class="page-content">
+            <section class="graym">
+                <h1>:-(</h1>
+                <h2>
+                    Erreur :
+                    <?php
+                    if ($erreur) {
+                        print $erreur;
+                    } else {
+                        print "Serveur TPG indisponible";
+                    }
+                    ?>
+                </h2>
+            </section>
+        </div>
     </div>
-</div>
-<?php } ?>
+<?php } ?>
