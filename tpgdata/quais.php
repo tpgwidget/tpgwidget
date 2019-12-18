9cad95445856b64622a168647ec975c1c518c2e5
Nicolas Ettlin <nicolas.ettlin@me.com>
2019-12-18 11:58:05 +0100

    Deprecate platforms feature (dataset obsolete)

--- tpgdata/quais.php
+++ tpgdata/quais.php
@@ -3,41 +3,7 @@ function qprint($quai) {
     echo '<div class="quai">'.htmlentities($quai).'</div>';
 }
 
-function quai($stopCode, $departureLine, $departureDestination) {
-    switch ($stopCode) {
-        case 'ESRT':
-            switch($departureDestination) {
-                case 'Lancy-Hubert': case 'Rive':
-                    qprint('1');
-                    break;
-                case 'Cressy': case 'Onex': case 'Onex-Salle communale': case 'Jardin Botanique':
-                    qprint('2');
-                    break;
-                case 'Aéroport':
-                    if ($departureLine == '23') {
-                        qprint('3');
-                    } elseif ($departureLine == "28") {
-                        qprint('2');
-                    }
-                case 'Chancy-Douane':
-                    if ($departureLine == "K") {
-                        qprint('3');
-                    } elseif ($departureLine == "NJ") {
-                        qprint('2');
-                    }
-                    break;
-                case 'Gare des Eaux-Vives': case 'Carouge-Tours': case 'Le Rolliet': case 'ZIPLO': case 'Ziplo':  case 'Pougny-Gare': case 'Avusy':
-                    qprint('3');
-                    break;
-                case 'Nations': case 'Cité Lignon': case 'Aéroport-P47':
-                    qprint('4');
-                    break;
-                default:
-                    if ($departureLine == "14") { // inclure tous les parcours de 14 + rentrées de dépôt
-                        qprint('1');
-                    }
-                    break;
-            }
-            break;
-    }
-}
+/**
+ * @deprecated
+ */
+function quai($stopCode, $departureLine, $departureDestination) {}
