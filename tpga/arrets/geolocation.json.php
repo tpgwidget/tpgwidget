8c3201c0382b31f7700d884baec317370743bde2
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-10-16 11:14:00 -0400

    Get rid of HTTPS calls everywhere

--- tpga/arrets/geolocation.json.php
+++ tpga/arrets/geolocation.json.php
@@ -2,7 +2,7 @@
 require_once __DIR__.'/../../config.inc.php';
 use TPGwidget\Data\Stops;
 
-$file = 'https://prod.ivtr-od.tpg.ch/v1/GetStops.xml?key='.getenv('TPG_API_KEY').'&latitude='.($_GET['latitude'] ?? '').'&longitude='.($_GET['longitude'] ?? '');
+$file = 'http://prod.ivtr-od.tpg.ch/v1/GetStops.xml?key='.getenv('TPG_API_KEY').'&latitude='.($_GET['latitude'] ?? '').'&longitude='.($_GET['longitude'] ?? '');
 $stops = @simplexml_load_file($file);
 
 $output = [];
