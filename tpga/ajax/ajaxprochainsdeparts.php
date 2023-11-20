86f3c9b26863ef4d5353cd2ea3feb583040daf9a
Adam Mathieson <adam@mathieson.dev>
2023-11-20 11:12:54 +0000

    + Circular lines go in circles

--- tpga/ajax/ajaxprochainsdeparts.php
+++ tpga/ajax/ajaxprochainsdeparts.php
@@ -44,7 +44,9 @@ include '../../tpgdata/vehicules/vehicules.php';
             ?>
                 <div class="item-inner">
                     <div class="item-title-row">
-                        <div class="item-title"><?= $depart->ligne ?> ➜ <?= Stops::format($depart->destination) ?></div>
+                        <div class="item-title"><?= $depart->ligne ?> ➜ <?= Stops::format($depart->destination) ?>
+                            <?= $depart->ligne == 47 ? '↻' : '' ?>
+                            <?= $depart->ligne == 48 ? '↺' : '' ?></div>
                         <div class="item-after">
                             <?php if ($depart->attente != "no more") {
                                 echo date("H:i", strtotime($depart->heureArrivee));
