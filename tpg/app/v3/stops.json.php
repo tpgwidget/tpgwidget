c384756c5ac996cc722b1e989c9e4fedf81a79b6
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-07-27 01:58:56 +0200

    Update the stops API on iOS

--- tpg/app/v3/stops.json.php
+++ tpg/app/v3/stops.json.php
@@ -2,70 +2,11 @@
 require_once __DIR__.'/../../../config.inc.php';
 use TPGwidget\Data\{Lines, Stops};
 
-function error() {
-    http_response_code(500);
-    echo json_encode(['error' => 'Server error']);
-    die();
-}
-
-//$fileUrl = 'http://prod.ivtr-od.tpg.ch/v1/GetPhysicalStops.json?key='.getenv('TPG_API_KEY');
-
-// Fall Back To Old API - This marks the final moments of TPGw and Third Party TPG Open Data Apps
-$fileUrl = "http://prod.ivtr.tpg.ch/GetTousArrets.json?transporteur=All";
-
-$fileContents = @file_get_contents($fileUrl);
-if (!$fileContents) {
-    error();
-}
-
-$stops = json_decode(file_get_contents("http://prod.ivtr.tpg.ch/GetTousArrets.json?transporteur=All"))->connexions->connexion;
-if (!$stops) {
-    error();
-}
-
 header('Content-type: application/json; charset=utf-8');
+echo json_encode([
+    'error' => "Pendant près de 10 ans, TPGwidget a aidé les Genevois·es à se déplacer. Aujourd’hui, les TPG ont supprimé la source d’informations dont TPGwidget dépendait, ce qui m’oblige à contrecœur à mettre mon application hors service.
 
-const FEATURED_STOPS = ['CVIN', 'BAIR', 'RIVE', 'AERO', 'COUT', 'PLPA', 'NATI', 'JOCT', 'CRGE', 'ESRT', 'HOPI', 'BHET'];
-$output = ['featured' => [], 'all' => [], 'error' => null];
+J’ai créé TPGwidget lorsque j’avais 13 ans pour réaliser mon rêve de gosse de créer une app. J’ai appris énormément grâce à ce projet et je tiens à vous remercier du fond du cœur pour votre soutien.
 
-// Stops list
-$byStopCode = [];
-foreach ($stops as $stop) {
-    $lines = [];
-
-    // Lines
-    $lineCodes = explode(',', strval($stop->lignes ?? $stop->ligne));
-    sort($lineCodes);
-    $lines = array_map(function ($lineCode) {
-        return Lines::get($lineCode);
-    }, $lineCodes);
-
-    $nameFormatted = Stops::format($stop->nomArret ?? '');
-    $stopData = [
-        'id' => $stop->codeArret ?? null,
-        'name' => [
-            'formatted' => $nameFormatted,
-            'corrected' => strip_tags($nameFormatted),
-            'raw' => $stop->nomArret,
-        ],
-        'lines' => $lines,
-    ];
-
-    $output['all'][] = $stopData;
-    $byStopCode[$stop->codeArret ?? ''] = $stopData;
-}
-
-// Sort stops
-uasort($output['all'], function($a, $b) {
-    return strcmp(strip_tags($a['name']['formatted']), strip_tags($b['name']['formatted']));
-});
-$output['all'] = array_values($output['all']);
-
-// Featured stops
-foreach (FEATURED_STOPS as $featured) {
-    if (array_key_exists($featured, $byStopCode)) {
-        $output['featured'][] = $byStopCode[$featured];
-    }
-}
-
-echo json_encode($output);
+— Nicolas Ettlin, créateur de TPGwidget"
+]);
